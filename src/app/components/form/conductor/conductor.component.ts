8d8ea8787780de3712fb4bea6952f950868933aa
hltav <hudsonlimatavares@gmail.com>
2023-08-22 10:27:59 -0300

    Implement Services and styles

--- src/app/components/form/conductor/conductor.component.ts
+++ src/app/components/form/conductor/conductor.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConductorService } from 'src/app/services/conductor.service';
 
 @Component({
   selector: 'app-conductor',
   templateUrl: './conductor.component.html',
   styleUrls: ['./conductor.component.sass']
 })
-export class ConductorComponent {
+export class ConductorComponent implements OnInit {
+
+  form : any 
+
+
+  constructor(private conductorService: ConductorService) { }
+
+  ngOnInit(): void {
+    this.getConductors();
+  }
+
+  getConductors(): void {
+    this.conductorService.getConductors().subscribe(
+      (data) => {
+        console.log(data); // Faça o que quiser com os dados recebidos
+      },
+      (error) => {
+        console.error('Erro ao obter condutores:', error);
+      }
+    );
+  }
+
+  createConductor(): void {
+    const newConductor = {
+      // Preencha com os dados do novo condutor
+    };
+
+    this.conductorService.createConductor(newConductor).subscribe(
+      (response) => {
+        console.log('Conductor criado com sucesso:', response);
+      },
+      (error) => {
+        console.error('Erro ao criar condutor:', error);
+      }
+    );
+  }
+
+  submitForm(formData: any): void {
+    // Chame o método do serviço para criar o condutor
+    this.conductorService.createConductor(formData).subscribe(
+      (response) => {
+        console.log('Conductor criado com sucesso:', response);
+        // Faça algo após o sucesso, como exibir uma mensagem ao usuário
+      },
+      (error) => {
+        console.error('Erro ao criar condutor:', error);
+        // Lide com o erro, como exibir uma mensagem de erro ao usuário
+      }
+    );
+  }
 
 }
---
+++ src/app/services/conductor.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConductorService {
+
+  private baseUrl = 'http://localhost:8000/api/'; 
+
+  constructor(private http: HttpClient) { }
+
+  getConductors(): Observable<any> {
+    return this.http.get(this.baseUrl + 'conductor');
+  }
+
+  createConductor(data: any): Observable<any> {
+    return this.http.post(this.baseUrl + 'conductor', data);
+  }
+}
