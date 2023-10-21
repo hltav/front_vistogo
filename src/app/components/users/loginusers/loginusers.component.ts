bdd1de2415e28f66faec91580f57969d0229811f
hltav <hudsonlimatavares@gmail.com>
2023-10-21 12:11:16 -0300

    commit created login page

--- src/app/components/users/loginusers/loginusers.component.ts
+++ src/app/components/users/loginusers/loginusers.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-loginusers',
   templateUrl: './loginusers.component.html',
-  styleUrls: ['./loginusers.component.sass']
+  styleUrls: ['./loginusers.component.sass'],
 })
 export class LoginusersComponent {
+  email!: string;
+  password!: string;
+  constructor(private router: Router, private userservice: UserService) {}
 
+  loginUser() {
+    const credentials = {
+      email: this.email,
+      password: this.password,
+    };
+
+    // this.userservice.findUserByEmail(credentials).subscribe(
+    //   () => {
+    //     localStorage.setItem('email', this.email);
+    //     localStorage.setItem('password', this.password);
+    //     alert('Login realizado com sucesso!');
+    //     setTimeout(() => {
+    //       this.router.navigate(['/home/inicial']);
+    //     }, 2000);
+    //   },
+    //   (error) => {
+    //     if (error.status === 401) {
+    //       alert('Usuário ou senha inválidos!');
+    //       console.log(error);
+    //     } else {
+    //       alert(
+    //         'Erro ao autenticar usuário: Por favor, aguarde uns instantes!'
+    //       );
+    //     }
+    //   }
+    // );
+    this.userservice.findUserByEmail(credentials.email).subscribe(
+      (user) => {
+        if (user && user.password === credentials.password) {
+          localStorage.setItem('email', this.email);
+          localStorage.setItem('password', this.password);
+          alert('Login realizado com sucesso!');
+          setTimeout(() => {
+            this.router.navigate(['/home/inicial']);
+          }, 2000);
+        } else {
+          alert('Usuário ou senha inválidos!');
+        }
+      },
+      (error) => {
+        if (error.status === 401) {
+          alert('Usuário ou senha inválidos!');
+          console.log(error);
+        } else {
+          alert('Erro ao autenticar usuário: Por favor, aguarde uns instantes!');
+        }
+      }
+    );
+  }
 }
